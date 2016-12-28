79854729af0ffd54ed09ca1b93bdead599d29fe7
hiword <28737164@qq.com>
2016-12-28 13:49:39 +0800

    add static

--- src/AbstractAttributeContract.php
+++ src/AbstractAttributeContract.php
@@ -8,11 +8,27 @@ use CrCms\AttributeContract\Exceptions\NotFoundException;
  */
 abstract class AbstractAttributeContract
 {
+    /**
+     * @var bool
+     */
+    protected $openException = false;
+
 
     /**
      * @return array
      */
     abstract public function getAttributes() : array ;
+
+
+    /**
+     * @param bool $status
+     * @return AbstractAttributeContract
+     */
+    public function setOpenException(bool $status) : AbstractAttributeContract
+    {
+        $this->openException = $status;
+        return $this;
+    }
 
 
     /**
@@ -22,12 +38,26 @@ abstract class AbstractAttributeContract
      */
     public function getTransform($key): string
     {
-        if (!isset($this->getAttributes()[$key]))
+        if (!isset($this->getAttributes()[$key]) && $this->openException)
         {
             throw new NotFoundException('No conversion value was found');
         }
 
-        return $this->getAttributes()[$key];
+        return $this->getAttributes()[$key] ?? '';
+    }
+
+
+    /**
+     * @param $name
+     * @param $arguments
+     * @return mixed
+     */
+    public static function __callStatic($name, $arguments)
+    {
+        $static = new static;
+        if (method_exists($name,$static)) {
+            return call_user_func_array([$static,$name],$arguments);
+        }
     }
 
 }
